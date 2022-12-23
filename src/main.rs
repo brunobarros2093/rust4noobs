34b37a3ca4c999f9103e070471881a6b564305c4
brunog.barros <brunog.barros@fornecedores.sicoob.com.br>
2022-12-23 14:58:28 -0300

    debug trait

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,6 @@ fn main() {
     // posições
     println!("{0} has index 0 - {2} and {1}", "pos0", "pos1", "pos2");
     println!("binario {:b}, Hex: {:x}, octal: {:o}", 5, 15, 55);
+    // debug trait - converte para uma string e printa
+    println!("Array: {:?}", [1,2,3]);
 }
