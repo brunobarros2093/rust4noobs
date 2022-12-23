8a9153bea838d9aedef13e260783ab17ff494a37
brunog.barros <brunog.barros@fornecedores.sicoob.com.br>
2022-12-23 14:37:55 -0300

    Entrada de usuário simples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // mutable ------------- cria uma string
+    let mut input = String::new();
+    println!("added something: ");
+
+    // pattern matching
+    match io::stdin().read_line(&mut input) {
+        // a linha do "match" pode retornar um OK ou um Error
+        Ok(_) => {
+            println!("said: {}", input);
+        },
+        Err(e) => {
+            println!("error {}", e);
+        }
+    }
 }
